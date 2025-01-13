30541ef3079d239c8e1d182adb279466c5bee7dd
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2025-01-13 13:21:36 +0530

    day 1

---
+++ 1 Patterns/Pattern7.java
@@ -0,0 +1,17 @@
+
+public class Pattern7 {
+  public static void pattern7(int n) {
+    for (int i = 0; i <= n; i++) {
+      for (int j = n; j <= i; j--) {
+        System.out.print(j);
+      }
+      System.out.println("");
+    }
+  }
+
+  public static void main(String[] args) {
+    pattern7(4);
+
+  }
+
+}
